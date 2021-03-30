b0d5483bd36fc94541507ca3329994368d710f34
veera_1505 <veerasaraha@gmail.com>
2021-03-30 19:29:26 +0530

    refactoring components and styling

---
+++ src/component/Income.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Income = () => {
+  return (
+    <div className='box'>
+      <p>Income</p>
+      <span>Rs 1000</span>
+    </div>
+
+  )
+}
+
+export default Income
