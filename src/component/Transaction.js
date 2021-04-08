717ce66f7c28718e2d3742e85302d4e1234ab3b1
veera_1505 <veerasaraha@gmail.com>
2021-04-08 19:57:41 +0530

    bug fixed

--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,19 +1,38 @@
 import React, { useContext } from 'react'
-import {ExpenseContext} from './../stateManagement/ExpenseProvider'
+import { ExpenseContext } from './../stateManagement/ExpenseProvider'
 
-const Transaction = ({transaction}) => {
-  const {removeTransaction} = useContext(ExpenseContext)
-  const sign = transaction.amount > 0 ? '+' : null 
+const Transaction = ({ transaction }) => {
+  const { removeTransaction } = useContext(ExpenseContext)
+  const sign = transaction.amount > 0 ? '+' : null
 
   return (
-      <li className='transaction' >
-        <span className={transaction.role === 'income' ? 'income' :'expense'}></span>
-        <span className='transaction-title' >{transaction.title}</span>
-        <span className='transaction-amount'>Rs.{sign}{transaction.amount.toFixed(2)}</span>
-        <button className='dlt-trn' onClick={ () => removeTransaction(transaction.id)}>
-          <i className="fas fa-times"></i>
+    <li className='transaction'>
+      <div className='child1'>
+        <span>{transaction.title}</span>
+      </div>
+
+      <div className='child2'>
+        <span
+          className={
+            transaction.role === 'income' ? 'income' : 'expense'
+          }></span>
+      </div>
+
+      <div className='child3'>
+        <span>
+          Rs.{sign}
+          {transaction.amount.toFixed(2)}
+        </span>
+      </div>
+
+      <div className='child4'>
+        <button
+          className='dlt-btn'
+          onClick={() => removeTransaction(transaction.id)}>
+          <i className='fas fa-times'></i>
         </button>
-      </li>
+      </div>
+    </li>
   )
 }
 
--- src/component/TransactionForm.js
+++ src/component/TransactionForm.js
@@ -4,53 +4,53 @@ import { ExpenseContext } from '../stateManagement/ExpenseProvider'
 const TransactionForm = () => {
   const { addTransaction } = useContext(ExpenseContext)
   const [data, setData] = useState({
-    title: "",
-    amount: ""
+    title: '',
+    amount: '',
   })
 
   const setRoleProperty = (obj) => {
-    const {amount} = obj
-    if(amount.startsWith('-')) {
+    const { amount } = obj
+    if (amount.startsWith('-')) {
       obj.role = 'expense'
     } else {
       obj.role = 'income'
     }
-  } 
+  }
 
-  
   const handleSubmit = (event) => {
     event.preventDefault()
     setRoleProperty(data)
-    addTransaction(data)  
-    setData({title : "", amount : ""})
+    addTransaction(data)
+    setData({ title: '', amount: '' })
   }
 
   return (
     <div>
-    <div className='form-header'>
-      <h2>Add new transaction </h2>
-      <span>( positive - income, negative - expense )</span>
-    </div>
-    <hr />
-    <form onSubmit={handleSubmit} className='form-container'>
+      <div className='form-header'>
+        <h2>Add new transaction </h2>
+        <span>( positive - income, negative - expense )</span>
+      </div>
+      <hr />
+      <form onSubmit={handleSubmit} className='form-container'>
+        <input
+          type='text'
+          placeholder='Add title'
+          required
+          value={data.title}
+          onChange={(e) => setData({ ...data, title: e.target.value })}
+          autoFocus
+        />
 
-      <input
-      type='text' 
-      placeholder='Add title'  
-      required
-      value={data.title} 
-      onChange={e => setData({...data, title : e.target.value})} 
-      autoFocus />
+        <input
+          type='number'
+          placeholder='Enter an amount'
+          value={data.amount}
+          required
+          onChange={(e) => setData({ ...data, amount: e.target.value })}
+        />
 
-      <input 
-      type='number' 
-      placeholder='Enter an amount' 
-      value={data.amount}  
-      required
-      onChange={e => setData({...data, amount : e.target.value})} />
-
-      <button type='submit'>submit</button>
-    </form>
+        <button type='submit'>submit</button>
+      </form>
     </div>
   )
 }
